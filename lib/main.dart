0466bd8255508df9552036d547558b576a016b8a
PaulMaltsev <paulmaltsev95@gmail.com>
2024-12-29 16:12:23 +0200

    Add riverpod lint, wrap root widget with riverpod provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:weather_one_digital/presentation/weather/weather_screen.dart';
 import 'package:weather_one_digital/presentation/weather_details/weather_details_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ProviderScope(
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
